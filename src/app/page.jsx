f492219a3da7d924c4a80fefe383581f321ec862
Gio <misael_marin@dlsu.edu.ph>
2023-11-26 23:25:20 +0800

    lcpimprovement

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -38,7 +38,7 @@ export default function Home() {
 								height={500}
 								className="object-cover w-full h-full "
 								alt="pfp"
-								loading="lazy"
+								priority
 							></Image>
 						</div>
 						<div className="mx-3 animate-slide-right">
