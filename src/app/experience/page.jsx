34d853e59dd3603047f324ba960eac83836103f6
Gio <misael_marin@dlsu.edu.ph>
2023-11-26 20:27:08 +0800

    jsx

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -1,4 +1,11 @@
-export default function Projects() {
+import React from "react";
+import Skills from "./Skills/Skills";
+import Experience from "./Experiences/experience";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
+const Experiences = () => {
 	return (
 		<div className="bg-black">
 			<div className="relative isolate px-6 pt-14 lg:px-8">
@@ -14,18 +21,30 @@ export default function Projects() {
 						}}
 					/>
 				</div>
-				<div className="mx-auto max-w-xl py-16 sm:py-20 lg:py-28">
-					<div className="mb-8 flex-col justify-center">
-                                                <iframe src="https://github.com/AndreiMarin15"></iframe>
-                                        </div>
-					<div className="text-center"></div>
+				<div className="mx-auto  py-8 sm:py-10 lg:py-14">
+					<div className="mb-4 flex justify-center text-center">
+						<div className="md:mx-4 flex">
+							<a
+								href="/assets/files/Resume-Andrei-Marin.pdf"
+								download
+								className="text-zinc-300 font-bold mb-2 hidden sm:block"
+							>
+								Resume {"	"}
+								<FontAwesomeIcon icon={faDownload} />
+							</a>
+						</div>
+					</div>
+					<div className="flex flex-col md:flex-row justify-center">
+						<Experience />
+						<Skills />
+					</div>
 				</div>
 				<div
 					className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
 					aria-hidden="true"
 				>
 					<div
-						className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ffffff] to-[#808080] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+						className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-zinc-200 to-zinc-600 opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
 						style={{
 							clipPath:
 								"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
@@ -35,4 +54,6 @@ export default function Projects() {
 			</div>
 		</div>
 	);
-}
+};
+
+export default Experiences;
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Sidebar from './navbar'
-import Navbar from "./navbar";
+import Navbar from "./navbar.jsx";
 
 const inter = Inter({ subsets: ['latin'] })
 
