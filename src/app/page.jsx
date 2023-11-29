6507b88d35bc2a52ab9b680ff0a66186fa396595
Gio <misael_marin@dlsu.edu.ph>
2023-11-29 12:57:20 +0800

    updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -29,7 +29,7 @@ export default function Home() {
 						}}
 					/>
 				</div>
-				<div className="mx-auto max-w-2xl py-8 sm:py-10 lg:py-14">
+				<div className="mx-auto py-8 sm:py-10 lg:py-14">
 					<div className="content items-center mb-8 flex justify-center">
 						<div className="w-24 h-24 mx-3 sm:w-44 sm:h-44 rounded-full overflow-hidden animate-slide-left">
 							<Image
