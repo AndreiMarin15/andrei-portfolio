bf38650a2608b477a17f8139df025783d5ffe3f3
AndreiMarin15 <andreimarin1622@gmail.com>
2024-07-08 02:22:08 +0800

    .

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -8,58 +8,58 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Tabs } from "@/components/ui/tabs";
 import { ExperienceTabs } from "@/components/templates/ExperienceTabs";
 const Experiences = () => {
-	return (
-		<div className="bg-black h-[100dvh] sm:h-[100vh]">
-			<div className="max-w-screen">
-				<div className="relative isolate px-6 pt-14 lg:px-8">
-					<div
-						className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-						aria-hidden="true"
-					>
-						<div
-							className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#008080] to-[#20b2aa] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-							style={{
-								clipPath:
-									"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-							}}
-						/>
-					</div>
-					<div className="mx-auto  py-8 sm:py-10 lg:py-14">
-						<div className="mb-4 flex justify-center text-center">
-							<div className="md:mx-4 flex animate-slide-top">
-								<a
-									href="/assets/files/Andrei_Marin_Resume.pdf"
-									target="_blank"
-									className="text-zinc-300 font-bold mb-2 "
-								>
-									View Resume {"	"}
-									<FontAwesomeIcon icon={faSearch} />
-								</a>
-							</div>
-						</div>
+  return (
+    <div className="bg-black h-[100dvh] sm:h-[100vh]">
+      <div className="max-w-screen">
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+          <div
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#008080] to-[#20b2aa] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
+          <div className="mx-auto  py-8 sm:py-10 lg:py-14">
+            <div className="mb-4 flex justify-center text-center">
+              <div className="md:mx-4 flex animate-slide-top">
+                <a
+                  href="/assets/files/Andrei_Marin_Resume.pdf"
+                  target="_blank"
+                  className="text-zinc-300 font-bold mb-2 "
+                >
+                  View Resume {"	"}
+                  <FontAwesomeIcon icon={faSearch} />
+                </a>
+              </div>
+            </div>
 
-						<div className="flex flex-col md:flex-row justify-center">
-							<ExperienceTabs />
-							{/* <Experience />
+            <div className="flex flex-col md:flex-row justify-center">
+              <ExperienceTabs />
+              {/* <Experience />
 							<Skills /> */}
-						</div>
-					</div>
-					<div
-						className="absolute inset-x-0 top-[calc(100%-30rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-47rem)]"
-						aria-hidden="true"
-					>
-						<div
-							className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-teal-200 to-teal-400 opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-							style={{
-								clipPath:
-									"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-							}}
-						/>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            </div>
+          </div>
+          <div
+            className="absolute inset-x-0 top-[calc(100%-30rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-47rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-teal-200 to-teal-400 opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Experiences;
